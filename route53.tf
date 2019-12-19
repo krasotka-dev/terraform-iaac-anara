11a3b497b8dfdf8ff641a316a4aaa93df46f280c
farrukh90 <farrukhsadykov@gmail.com>
2019-12-18 19:20:09 -0600

    added zone ID

---
+++ route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "www" {
+  zone_id = "Z32OHGRMBVZ9LR"
+  name    = "www.acirrustech.com"
+  type    = "A"
+  ttl     = "60"
+  records = [aws_instance.web.public_ip]
+}
