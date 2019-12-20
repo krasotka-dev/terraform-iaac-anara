c373eb20f5ecba952155a8217f45640c59b9af10
farrukh90 <farrukhsadykov@gmail.com>
2019-12-20 14:00:53 -0600

    added verssion

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -3,6 +3,7 @@ module "my-cluster" {
   cluster_name = "dev"
   subnets      = ["subnet-e3a93a85", "subnet-3425477c", "subnet-104d904a"]
   vpc_id       = "vpc-908caaf6"
+  cluster_version = "1.14"
 
   worker_groups = [
     {
