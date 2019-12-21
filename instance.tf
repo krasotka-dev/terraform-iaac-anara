b43a60357a898602abb9cd50b968e81717083cfc
farrukh90 <farrukhsadykov@gmail.com>
2019-12-21 12:28:04 -0600

    removed space

--- instance.tf
+++ instance.tf
@@ -6,18 +6,18 @@ resource "aws_instance" "web" {
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
   provisioner "remote-exec" {
-    connection {
-      host        = self.public_ip
-      type        = "ssh"
-      user        = var.user
-      private_key = file(var.ssh_key_location)
-    }
-    inline = [
-      "sudo yum install -y epel-release",
-      "sudo yum install httpd -y ",
-      "systemctl start httpd",
-    ]
-  }
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = var.user
+      private_key = file(var.ssh_key_location)
+      }
+      inline = [
+        "sudo yum install -y epel-release",
+        "sudo yum install httpd -y ",
+        "systemctl start httpd",
+      ]
+      }
   lifecycle{
     prevent_destroy = false
   }
