dcd042b0a6b9b20d9423c1d61740e8cbca0ad97d
farrukh90 <farrukhsadykov@gmail.com>
2019-12-20 19:18:40 -0600

    formated

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -1,22 +1,22 @@
 module "my-cluster" {
-  source       = "./terraform-aws-eks"
-  cluster_name = "dev"
-  subnets      = ["subnet-c74d1e8d", "subnet-e4820eb8", "subnet-1fe46778"]
-  vpc_id       = "vpc-1471ad6e"
-  cluster_version = "1.14"
+  source                    = "./terraform-aws-eks"
+  cluster_name              = "dev"
+  subnets                   = ["subnet-c74d1e8d", "subnet-e4820eb8", "subnet-1fe46778"]
+  vpc_id                    = "vpc-1471ad6e"
+  cluster_version           = "1.14"
   cluster_security_group_id = "sg-0baae22921460195f"
 
   worker_groups = [{
-      instance_type = "m4.large"
-      asg_max_size  = 15
-      asg_min_size  = 3
-      asg_desired_capacity = 3
-      key_name      = "farrukh's_laptop"
-      tags = [{
-        key                 = "foo"
-        value               = "bar"
-        propagate_at_launch = true
-      }]
+    instance_type        = "m4.large"
+    asg_max_size         = 15
+    asg_min_size         = 3
+    asg_desired_capacity = 3
+    key_name             = "farrukh's_laptop"
+    tags = [{
+      key                 = "foo"
+      value               = "bar"
+      propagate_at_launch = true
+    }]
     }
   ]
 
