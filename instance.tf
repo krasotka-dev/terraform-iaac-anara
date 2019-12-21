930dd4b86d06eab28acb06c329609507f7d397e8
farrukh90 <farrukhsadykov@gmail.com>
2019-12-21 12:29:37 -0600

    removed space

--- instance.tf
+++ instance.tf
@@ -7,9 +7,9 @@ resource "aws_instance" "web" {
   security_groups = ["allow_ssh"]
   provisioner "remote-exec" {
     connection {
-      host        = self.public_ip
-      type        = "ssh"
-      user        = var.user
+      host = self.public_ip
+      type = "ssh"
+      user = var.user
       private_key = file(var.ssh_key_location)
       }
       inline = [
