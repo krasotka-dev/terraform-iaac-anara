2eec1cdb813454456b65a401224cc5bf67e20768
farrukh90 <farrukhsadykov@gmail.com>
2020-01-08 18:35:47 -0600

    added new data source

---
+++ data_source/ec2.tf
@@ -0,0 +1,21 @@
+# Go to Region us-east-2 
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+
+# Search for Ubuntu Latest with the owner
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+}
+
+
+
+
+
+# Show the AMI id 
+output "ami" {
+  value = "${data.aws_ami.ubuntu.id}"
+}
