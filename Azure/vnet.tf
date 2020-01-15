792b8eaab75661fe8aa964d7a457bc17440ffd6d
farrukh90 <farrukhsadykov@gmail.com>
2020-01-15 09:08:33 -0600

    added proper subnet

--- Azure/vnet.tf
+++ Azure/vnet.tf
@@ -9,5 +9,5 @@ resource "azurerm_subnet" "private" {
     name = "private"
     resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
     virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
-    address_prefix = "10.0.0.1/24"
+    address_prefix = "10.0.1.0/24"
 }
