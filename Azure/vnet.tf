8dffc506204d9d2d708ce06b1a4752ed35f0c041
farrukh90 <farrukhsadykov@gmail.com>
2020-01-15 09:13:20 -0600

    added s2cond

--- Azure/vnet.tf
+++ Azure/vnet.tf
@@ -11,3 +11,10 @@ resource "azurerm_subnet" "private" {
     virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
     address_prefix = "10.0.1.0/24"
 }
+
+resource "azurerm_subnet" "private2" {
+    name = "private2"
+    resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+    virtual_network_name = "${azurerm_virtual_network.vnet1.name}"
+    address_prefix = "10.0.2.0/24"
+}
