58a952c71b9a284daae0d75a6e514ef4ab5a4381
farrukh90 <farrukhsadykov@gmail.com>
2020-01-15 10:04:45 -0600

    added os

--- Azure/vnet.tf
+++ Azure/vnet.tf
@@ -60,3 +60,35 @@ resource "azurerm_network_security_group" "sec_group1" {
     environment = "Production"
   }
 }
+
+
+resource "azurerm_virtual_machine" "vm1" {
+  name = "vm1"
+  location = "westus2"
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.nic1.id}"]
+  vm_size = "Standard_DS1_v2"
+  storage_image_reference {
+    publisher = "OpenLogic"
+    offer = "CentOS"
+    sku = "7.5"
+    version = "latest"
+  }
+  storage_os_disk {
+    name = "myosdisk1"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+   managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags  {
+    environment = "staging"
+  }
+}
