27d90e12976069864c6e503c30a923eed170c63e
farrukh90 <farrukhsadykov@gmail.com>
2020-01-15 10:19:52 -0600

    added more resources

--- Azure/vnet.tf
+++ Azure/vnet.tf
@@ -62,6 +62,29 @@ resource "azurerm_network_security_group" "sec_group1" {
 }
 
 
+
+# vm1" {
+resource "azurerm_network_interface" "nic1" {
+  name = "nic1"
+  location = "westus2"
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+  ip_configuration {
+    name = "testconfiguration1"
+    subnet_id = "${azurerm_subnet.private.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = "${azurerm_public_ip.IP.id}"
+  }
+}
+
+resource "azurerm_public_ip" "IP" {
+    name = "public_ip"
+    location = "westus2"
+    resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+    allocation_method = "Dynamic"
+}
+
+
+
 resource "azurerm_virtual_machine" "vm1" {
   name = "vm1"
   location = "westus2"
@@ -93,6 +116,27 @@ resource "azurerm_virtual_machine" "vm1" {
   }
 }
 
+
+
+# VM 2
+resource "azurerm_network_interface" "nic2" {
+  name = "nic2"
+  location = "westus2"
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+  ip_configuration {
+    name = "testconfiguration2"
+    subnet_id = "${azurerm_subnet.private.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = "${azurerm_public_ip.IP.id}"
+  }
+}
+resource "azurerm_public_ip" "IP2" {
+    name = "public_ip2"
+    location = "westus2"
+    resource_group_name = "${azurerm_resource_group.web_server_rg.name}"
+    allocation_method = "Dynamic"
+}
+
 resource "azurerm_virtual_machine" "vm2" {
   name = "vm2"
   location = "westus2"
@@ -106,7 +150,7 @@ resource "azurerm_virtual_machine" "vm2" {
     version = "latest"
   }
   storage_os_disk {
-    name = "myosdisk1"
+    name = "myosdisk2"
     caching = "ReadWrite"
     create_option = "FromImage"
    managed_disk_type = "Standard_LRS"
