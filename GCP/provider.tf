4631feb2718694aebd4a2651de3d94aa638d3af1
farrukh90 <farrukhsadykov@gmail.com>
2020-01-15 21:09:28 -0600

    fixed

--- GCP/provider.tf
+++ GCP/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("credentials.json"
+  credentials = "${file("credentials.json")}"
   project     = "handy-reference-265302"
   region      = "us-central1"
 }
